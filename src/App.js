5b1be31ed80b9a325903c59bf13badaaba6dcc77
user_name <email_id>
2023-03-31 16:25:28 -0400

    l

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import AllocationForm from './components/AllocationForm';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -14,19 +17,29 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <ExpenseList />
+                    </div>
+                    {/* <div className='col'>
+                        <ExpenseItem />
+                    </div> */}
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
